b5840602acf09a0cabd2485cc0995e19e3bf1591
RikersMax <maksimlarionov112@icloud.com>
2023-05-03 18:59:36 +0500

    'Visti-params'

--- app.rb
+++ app.rb
@@ -14,3 +14,22 @@ end
 get '/visit' do
    	erb :visit
 end
+
+get '/services' do
+   	erb :services
+end
+
+post '/visit-client' do
+   	@user_fname_client = params[:user_fname]
+	@user_lnaem_client = params[:user_lname]
+	@user_phone_client = params[:user_phone]
+	@user_time_client = params[:user_time]
+	@user_gender_client = params[:gender]
+	@pairdresser_name_client = params[:barber_name]
+	@user_text_client = params[:user_text]
+
+
+	erb("Good! <p><b>#{@user_fname_client.capitalize} #{@user_lnaem_client.capitalize}</b><p> 
+	you will come to visit us on <b>#{@user_time_client}</b> <p>Will be waiting for you</p> <b>#{@pairdresser_name_client}</b>")	
+	
+end
